6be422d1c1872b97f7bbf0072c74efa07d78f55d
Visal Prom <30904995+visalkoko@users.noreply.github.com>
2019-07-02 09:01:19 +0700

    array challenge

--- array.js
+++ array.js
@@ -27,3 +27,33 @@ console.log(john);
 console.log(john.indexOf('students'));
 var result = john.indexOf('designer') === -1 ? 'John is not desinger' : 'John is a designer';
 console.log(result);
+
+
+// Array Coding Challenges
+
+function tipBill(bill){
+    var percentage;
+    if (bill < 50){
+        percentage = .2;
+    }
+    else if (bill >=50 && bill <200){
+        percentage = .15;
+    }
+    else{
+        percentage = 0.1;
+    }
+    return bill * percentage;
+}
+
+console.log(tipBill(50));
+
+var bill = [30, 100, 300];
+var allTips = [tipBill(bill[0]), tipBill(bill[1]), tipBill(bill[2])];
+var finalPaid = [bill[0] + allTips[0],
+                 bill[1] + allTips[1],
+                 bill[2] + allTips[2],
+                ];
+
+console.log(allTips, finalPaid);
+
+
